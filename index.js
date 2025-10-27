4e695e3edfebf293bb1e3d68357e528e794aa03c
Stefanie <marystefaniebermudez@gmail.com>
2025-10-27 12:42:47 -0500

    Initialize Express server with basic setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT;
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`uola`);
+});
+
