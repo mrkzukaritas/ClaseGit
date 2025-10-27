00b67bff985336eea5ed018342a1dbe3ca3e652a
Jeryleefth <jeryleefth.lasso@estudiantesunibague.edu.co>
2025-10-27 13:03:22 -0500

    ..

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000 || process.env.PORT;
 app.use(express.json());
+
+app.use('/comics', require('./routes/comics'));
 app.listen(port, () => {
   console.log(`uola`);
 });
